0f54a3ac837c507ee34299958861af5a362e31af
Vijay Dudhal <89061368+vijay5121@users.noreply.github.com>
2023-10-06 14:12:34 +0000

    conversion and datatype

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,14 @@
+/*
+number => 2 to power 53
+bigint=>
+string=>""
+boolean=>true/false
+null=>standalone value
+undefiend
+symbol=>unique
+object
+*/
+
+console.log(typeof null); //object
+console.log(typeof undefined)//undefined
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=123
+let accountEmail="vijay@gmail.com"
+var accountPass="vijay#123"
+accountcity="pune"
+var accountPass=525
+console.log(accountId);
+
+/*preferd do not use var variables
+becoz of block scope and fuctional scope
+*/
+console.table([accountId,accountEmail,accountPass,accountPass])
